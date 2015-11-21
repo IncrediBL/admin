0bd8d1c1bee99a76960aa8de0c1c37f76c00fb03
Amanda Folson <zomg@Amandas-MacBook-Pro.local>
2015-11-21 12:40:29 -0500

    Initial dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+@include('partials.header')
+
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["geochart"]});
+      google.setOnLoadCallback(drawRegionsMap);
+
+      function drawRegionsMap() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Country', 'Popularity'],
+          ['Germany', 200],
+          ['United States', 300],
+          ['Brazil', 400],
+          ['Canada', 500],
+          ['France', 600],
+          ['RU', 700]
+        ]);
+
+        var options = {};
+
+        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+      <!-- Main component for a primary marketing message or call to action -->
+<!--       <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+        </p>
+      </div> -->
+      <div class="col-sm-12">
+        <div id="regions_div"></div>
+    </div>
+
+@include('partials.footer')
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+    </div> <!-- /container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/js/app.js"></script>
+  </body>
+</html>
